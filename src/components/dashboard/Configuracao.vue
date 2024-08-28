161b40911db00360549665856d434cec3d661103
GustavoM4tias <128002171+GustavoM4tias@users.noreply.github.com>
2024-08-27 21:52:47 -0300

    configuracoes

--- src/components/dashboard/Configuracao.vue
+++ src/components/dashboard/Configuracao.vue
@@ -1,14 +1,128 @@
 <script setup>
-
 </script>
 
 <template>
+    <div class="container-fluid p-0">
 
-    <div class="">Dashboard Configurações</div>
+    <div class="topo px-5 pt-4 pb-2">
+        <h3 class="fw-bold">Configuraçoes</h3>
+    </div>
 
+    <div class="cols px-3 d-flex justify-content-around">
+
+ 
+        <div class="col-md-4 col-12 p-2">
+            <div class="card p-4 frete">
+                <h4 class="titulo-card mb-3"><i class="fa-solid fa-truck"></i> Frete e envio</h4>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-md-4 col-12 p-2">
+            <div class="card p-4 frete">
+                <h4 class="titulo-card mb-3"><i class="fa-solid fa-truck"></i> Frete e envio</h4>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-md-4 col-12 p-2">
+            <div class="card p-4 frete">
+                <h4 class="titulo-card mb-3"><i class="fa-solid fa-truck"></i> Frete e envio</h4>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+                <div class="item">
+                    <h5 class="titulo">Correios</h5>
+                    <p class="texto">Com a plataforma dos correios tem varios bagui daora e etc.</p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
 </template>
 
 <style scoped>
+.container-fluid {
+    height: 92vh;
+    background-color: var(--background-secondary);
+}
 
+.cols {
+    height: 85vh;
+}
 
+.item {
+    background-color: var(--background-secondary);
+    border-radius: 10px;
+    padding: 10px;
+    margin: 10px 0 10px 0;
+}
+
+@media (max-width: 768px) {
+
+    .container {
+        max-width: 100% !important;
+    }
+
+    .cols {
+        flex-direction: column;
+    }
+
+}
 </style>

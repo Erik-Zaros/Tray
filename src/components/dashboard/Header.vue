3a93ba7236eae2d575c1792057c6b48aeb625332
Erik Delanda Zaros <erikzaros942@gmail.com>
2024-09-22 19:35:13 -0300

    Configuracao_Template

--- src/components/dashboard/Header.vue
+++ src/components/dashboard/Header.vue
@@ -1,217 +1,223 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 import { useRouter } from 'vue-router';
 import { logout } from '../../services/api';
 
 const router = useRouter();
 const menu = ref(false);
+const navbarColor = ref('#424242');
+
+onMounted(() => {
+  const storedColor = localStorage.getItem('navbarColor');
+  if (storedColor) {
+    navbarColor.value = storedColor;
+    atualizarCorNavbar(navbarColor.value);
+  }
+});
+
+const atualizarCorNavbar = (cor) => {
+  document.documentElement.style.setProperty('--navbar-color', cor);
+  localStorage.setItem('navbarColor', cor);
+};
+
+watch(navbarColor, (novaCor) => {
+  atualizarCorNavbar(novaCor);
+});
 
 function mudarMenu() {
-    menu.value = !menu.value;
+  menu.value = !menu.value;
 }
 
 const deslogar = async () => {
-    try {
-        await logout(); // Chama a função de logout da api.js
-        router.push('/login');
-    } catch (error) {
-        console.error('Erro ao fazer logout:', error);
-        alert('Erro ao fazer logout. Tente novamente.');
-    }
+  try {
+    await logout();
+    localStorage.removeItem('navbarColor'); // Limpa cor ao deslogar
+    router.push('/login');
+  } catch (error) {
+    console.error('Erro ao fazer logout:', error);
+    alert('Erro ao fazer logout. Tente novamente.');
+  }
 };
-
 </script>
 
 <template>
+  <div>
+    <div class="horizontal d-flex vw-100 justify-content-between px-4">
+      <div class="menu d-flex align-items-center">
+        <i id="tour-1" class="menu-icon fa-solid rounded fa-bars fs-1" @click="mudarMenu"></i>
+      </div>
 
-    <div>
+      <div class="d-flex align-items-center">
+        <router-link to="/dashboard/ajuda">
+          <i class="fa-regular fa-circle-question fs-3 px-2 px-md-3"></i>
+        </router-link>
 
-        <div class="horizontal d-flex vw-100 justify-content-between px-4">
-            <div class="menu d-flex align-items-center">
-                <i id="tour-1" class="menu-icon fa-solid rounded fa-bars fs-1" @click="mudarMenu">
-                </i>
-            </div>
-
-            <div class="d-flex align-items-center">
-
-                <router-link to="/dashboard/ajuda">
-                    <i class="fa-regular fa-circle-question fs-3 px-2 px-md-3"></i>
-                </router-link>
-
-                <router-link to="/dashboard/perfil">
-                    <i class="bi bi-person-fill fs-2 px-2 px-md-3"></i>
-                </router-link>
-            </div>
-        </div>
-
-        <div
-            :class="['vertical', 'd-flex', 'flex-column', 'align-items-between', 'justify-content-between', 'text-center', { expanded: menu }]">
-
-            <div class="content-1">
-                <label class="icon-wrapper my-1"><router-link to="/dashboard/inicio">
-                        <input type="radio" name="icon-selection" class="icon-radio" checked></router-link>
-                    <i id="tour-2" class="fa-regular icon-menu d-flex py-3 px-4 rounded fa-flag"><span
-                            class="text-icon px-2">Início</span></i>
-                </label>
-                <label class="icon-wrapper my-1"><router-link to="/dashboard/perfil">
-                        <input type="radio" name="icon-selection" class="icon-radio"></router-link>
-                    <i id="tour-3" class="fa-regular icon-menu d-flex py-3 px-4 rounded fa-user"><span
-                            class="text-icon px-2">Perfil</span></i>
-                </label>
-                <label class="icon-wrapper my-1"><router-link to="/dashboard/configuracao">
-                        <input type="radio" name="icon-selection" class="icon-radio"></router-link>
-                    <i id="tour-7" class="fa-solid icon-menu d-flex py-3 px-4 rounded fa-gear"><span
-                            class="text-icon px-2">Configurações</span></i>
-                </label>
-            </div>
-
-            <div class="content-2">
-                <label class="icon-wrapper my-1"><router-link to="/dashboard/produtos">
-                        <input type="radio" name="icon-selection" class="icon-radio"></router-link>
-                    <i id="tour-5" class="fa-solid icon-menu d-flex py-3 px-4 rounded fa-tag"><span
-                            class="text-icon px-2">Produtos</span></i>
-                </label>
-                <label class="icon-wrapper my-1"><router-link to="/dashboard/loja">
-                        <input type="radio" name="icon-selection" class="icon-radio"></router-link>
-                    <i id="tour-4" class="fa-solid icon-menu d-flex py-3 px-4 rounded fa-store"><span
-                            class="text-icon px-2">Sua Loja</span></i>
-                </label>
-                <label class="icon-wrapper my-1"><router-link to="/dashboard/templates">
-                        <input type="radio" name="icon-selection" class="icon-radio"></router-link>
-                    <i id="tour-6" class="fa-solid icon-menu d-flex py-3 px-4 rounded fa-tv"><span
-                            class="text-icon px-2">Templates</span></i>
-                </label>
-            </div>
-
-            <div class="content-3">
-                <label class="icon-wrapper my-1"><router-link to="/dashboard/ajuda">
-                        <input type="radio" name="icon-selection" class="icon-radio"></router-link>
-                    <i id="tour-8" class="fa-regular icon-menu d-flex py-3 px-4 rounded fa-circle-question"><span
-                            class="text-icon px-2">Ajuda</span></i>
-                </label>
-                <label class="icon-wrapper my-1" @click="deslogar">
-                    <input type="radio" name="icon-selection" class="icon-radio">
-                    <i id="tour-9" class="fa-solid icon-menu d-flex py-3 px-4 rounded fa-right-from-bracket"><span
-                            class="text-icon px-2">Logout</span></i>
-                </label>
-            </div>
-        </div>
+        <router-link to="/dashboard/perfil">
+          <i class="bi bi-person-fill fs-2 px-2 px-md-3"></i>
+        </router-link>
+      </div>
     </div>
+
+    <div
+      :class="['vertical', 'd-flex', 'flex-column', 'align-items-between', 'justify-content-between', 'text-center', { expanded: menu }]">
+
+      <div class="content">
+        <label class="icon-wrapper my-2">
+          <router-link to="/dashboard/inicio">
+            <i id="tour-2" class="fa-regular icon-menu fa-flag py-3 px-4 rounded"></i>
+            <span class="text-icon">Início</span>
+          </router-link>
+        </label>
+
+        <label class="icon-wrapper my-2">
+          <router-link to="/dashboard/perfil">
+            <i id="tour-3" class="fa-regular icon-menu fa-user py-3 px-4 rounded"></i>
+            <span class="text-icon">Perfil</span>
+          </router-link>
+        </label>
+
+        <label class="icon-wrapper my-2">
+          <router-link to="/dashboard/loja">
+            <i id="tour-4" class="fa-solid icon-menu fa-store py-3 px-4 rounded"></i>
+            <span class="text-icon">Sua Loja</span>
+          </router-link>
+        </label>
+
+        <label class="icon-wrapper my-2">
+          <router-link to="/dashboard/produtos">
+            <i id="tour-5" class="fa-solid icon-menu fa-tag py-3 px-4 rounded"></i>
+            <span class="text-icon">Produtos</span>
+          </router-link>
+        </label>
+
+        <label class="icon-wrapper my-2">
+          <router-link to="/dashboard/templates">
+            <i id="tour-6" class="fa-solid icon-menu fa-tv py-3 px-4 rounded"></i>
+            <span class="text-icon">Templates</span>
+          </router-link>
+        </label>
+
+        <label class="icon-wrapper my-2">
+          <router-link to="/dashboard/configuracao">
+            <i id="tour-7" class="fa-solid icon-menu fa-gear py-3 px-4 rounded"></i>
+            <span class="text-icon">Configurações</span>
+          </router-link>
+        </label>
+
+        <label class="icon-wrapper my-2">
+          <router-link to="/dashboard/ajuda">
+            <i id="tour-8" class="fa-regular icon-menu fa-circle-question py-3 px-4 rounded"></i>
+            <span class="text-icon">Ajuda</span>
+          </router-link>
+        </label>
+
+        <label class="icon-wrapper my-2" @click="deslogar">
+          <i id="tour-9" class="fa-solid icon-menu fa-right-from-bracket py-3 px-4 rounded"></i>
+          <span class="text-icon">Logout</span>
+        </label>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+:root {
+  --navbar-color: #424242; /* Cor padrão */
+}
+
 * {
-    color: white;
-    box-sizing: border-box;
+  color: white;
+  box-sizing: border-box;
 }
 
 html,
 body {
-    margin: 0;
-    padding: 0;
-    height: 100%;
+  margin: 0;
+  padding: 0;
+  height: 100%;
 }
 
 .content {
-    display: flex;
-    height: 100vh;
-    overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow: hidden;
 }
 
 .horizontal {
-    background-color: #424242;
-    height: 8vh;
-    width: 100%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 1000;
+  background-color: var(--navbar-color);
+  height: 8vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
 }
 
 .vertical {
-    background-color: #484848;
-    height: 92vh;
-    width: 80px;
-    position: fixed;
-    top: 8vh;
-    left: 0;
-    z-index: 999;
-    transition: width 0.3s;
+  background-color: var(--navbar-color);
+  height: 92vh;
+  width: 80px;
+  position: fixed;
+  top: 8vh;
+  left: 0;
+  z-index: 999;
+  transition: width 0.3s;
 }
 
 .vertical:hover,
 .vertical.expanded {
-    width: 200px;
-
-    & .text-icon {
-        display: block;
-    }
-}
-
-.content-wrapper {
-    margin-left: 80px;
-    margin-top: 8vh;
-    flex: 1;
-    overflow-y: auto;
-    padding: 16px;
-    background-color: #f4f4f4;
+  width: 200px;
 }
 
 .menu-icon {
-    transition: .2s;
+  transition: .2s;
 }
 
 .menu-icon:hover {
-    transform: scale(1.08);
-    transition: .2s;
-    cursor: pointer;
-}
-
-.search {
-    background-color: rgba(255, 255, 255, .6);
-    border: none;
-}
-
-.search-icon {
-    cursor: pointer;
-}
-
-.dropdown-menu {
-    background-color: #424242;
+  transform: scale(1.08);
+  transition: .2s;
+  cursor: pointer;
 }
 
 .icon-menu {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .text-icon {
-    display: none;
-    font-weight: normal;
+  display: none;
+  margin-left: 10px;
 }
 
-.icon-menu:hover {
-    background-color: rgb(0, 110, 255);
+.vertical.expanded .text-icon {
+  display: inline-block;
 }
 
 .icon-wrapper {
-    position: relative;
-    display: inline-block;
-    transition: .5s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: .3s;
 }
 
-.icon-radio {
-    position: absolute;
-    opacity: 0;
-    cursor: pointer;
+.icon-wrapper:hover {
+  background-color: rgba(0, 110, 255, 0.2);
+  border-radius: 10px;
 }
 
-.icon-radio:checked+.fa-solid {
-    background-color: rgb(0, 110, 255);
+.icon-menu {
+  font-size: 1.5rem;
+  color: white;
+  transition: .2s;
+}
+
+.icon-wrapper:hover .icon-menu {
+  transform: scale(1.1);
 }
 
 @media (max-width: 1024px) {
-    .search-input {
-        display: none;
-    }
+  .search-input {
+    display: none;
+  }
 }
 </style>

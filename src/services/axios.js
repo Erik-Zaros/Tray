00482cda9f5e05387e6313c0f406a036016a520e
Edmar <128000264+Edmar452@users.noreply.github.com>
2024-09-22 16:46:47 -0300

    Revert "Adicionando Back/Api"

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import store from '../store';
 
 const instance = axios.create({
-  baseURL: 'https://localhost:7205', 
+  baseURL: 'https://megev-affefxfrajedbqbw.brazilsouth-01.azurewebsites.net', 
   headers: {
     'Content-Type': 'application/json'
   }

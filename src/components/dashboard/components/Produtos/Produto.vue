a4267faaa767604d51289a776461c899b9ec6275
Erik Delanda Zaros <erikzaros942@gmail.com>
2024-09-29 16:15:41 -0300

    Correcao_Filtragem_Produto_Loja

--- src/components/dashboard/components/Produtos/Produto.vue
+++ src/components/dashboard/components/Produtos/Produto.vue
@@ -2,7 +2,6 @@
 import Excluir from './Excluir.vue';
 import { defineEmits, defineProps } from 'vue';
 
-// Propriedades que o componente recebe
 const produto = defineProps({
   image: String,
   referencia: String,
@@ -15,10 +14,8 @@ const produto = defineProps({
   selecionado: Boolean
 });
 
-// Definição dos eventos emitidos pelo componente
 const emit = defineEmits(['editarProduto', 'excluirProduto', 'selecionarProduto']);
 
-// Função para emitir o evento de edição do produto
 const emitirEditar = () => {
   emit('editarProduto', {
     id: produto.id,
@@ -32,18 +29,15 @@ const emitirEditar = () => {
   });
 };
 
-// Função para mostrar a confirmação de exclusão
 const mostrarConfirmacaoExclusao = (id) => {
   const modal = new bootstrap.Modal(document.getElementById('modalConfirmacaoExclusao'));
   modal.show();
 };
 
-// Função para emitir o evento de exclusão do produto
 const emitirExcluir = () => {
   emit('excluirProduto', produto.id);
 };
 
-// Emitir evento de seleção ao clicar no checkbox
 const alterarSelecao = (event) => {
   emit('selecionarProduto', {
     id: produto.id,
@@ -55,44 +49,36 @@ const alterarSelecao = (event) => {
 <template>
   <div class="produto-card px-5 mx-4 p-3">
     <div class="row align-items-center">
-      <!-- Checkbox para selecionar o produto -->
       <div class="col-auto me-4">
         <input class="form-check-input" type="checkbox" :checked="selecionado" @change="alterarSelecao">
       </div>
 
-      <!-- Exibição da referência do produto -->
       <div class="col-auto me-4">
         <p class="produto-referencia">{{ referencia }}</p>
       </div>
 
-      <!-- Exibição da imagem do produto -->
       <div class="col-auto me-4">
         <img class="produto-imagem" :src="image || '/noimg.jpeg'" alt="Imagem do Produto">
       </div>
 
-      <!-- Exibição da descrição do produto -->
       <div class="col me-auto">
         <p class="produto-descricao">{{ descricao }}</p>
       </div>
 
-      <!-- Exibição da categoria do produto -->
       <div class="col">
         <p class="produto-categoria">{{ categoria }}</p>
       </div>
 
-      <!-- Exibição do preço do produto -->
       <div class="col">
         <p class="produto-preco">R$ {{ preco.toFixed(2) }}</p>
       </div>
 
-      <!-- Exibição do status do produto (Ativo/Inativo) -->
       <div class="col">
         <span :class="['produto-status badge', status ? 'badge-success' : 'badge-danger']">
           {{ status ? 'Ativo' : 'Inativo' }}
         </span>
       </div>
 
-      <!-- Botões de editar e excluir o produto -->
       <div class="col-1 alterar d-flex justify-content-between">
         <div class="editar">
           <i class="fa-solid fa-pen-to-square fs-3" @click="emitirEditar"></i>
@@ -100,7 +86,6 @@ const alterarSelecao = (event) => {
         <div class="excluir">
           <i class="fa-solid fa-trash-can fs-3" @click="mostrarConfirmacaoExclusao(produto.id)"></i>
         </div>
-        <!-- Modal de exclusão -->
         <Excluir @confirmarExclusao="emitirExcluir" />
       </div>
     </div>
@@ -108,7 +93,6 @@ const alterarSelecao = (event) => {
 </template>
 
 <style scoped>
-/* Estilização do card do produto */
 .produto-card {
   color: #333;
   border-top: 2px solid rgba(140, 140, 140, .3);
@@ -122,17 +106,14 @@ const alterarSelecao = (event) => {
   border-radius: 8px;
 }
 
-/* Efeito de hover para ícones de edição e exclusão */
 .fa-solid:hover {
   cursor: pointer;
 }
 
-/* Configurações de layout das colunas */
 .col-auto {
   flex: 0 0 auto;
 }
 
-/* Definições de tamanho e estilo de texto */
 .produto-referencia,
 .produto-descricao,
 .produto-categoria,
@@ -162,23 +143,21 @@ const alterarSelecao = (event) => {
 
 .produto-status {
   width: 100px;
-  font-size: 1.1rem; /* Aumenta o tamanho da fonte */
-  padding: 8px 12px; /* Aumenta o padding interno */
-  border-radius: 0.5rem; /* Ajusta a borda arredondada */
+  font-size: 1.1rem; 
+  padding: 8px 12px; 
+  border-radius: 0.5rem; 
 }
 
-/* Estilo customizado dos badges (status Ativo e Inativo) */
 .produto-status.badge-success {
-  background-color: #28a745; /* Verde para ativo */
+  background-color: #28a745; 
   color: white;
 }
 
 .produto-status.badge-danger {
-  background-color: #dc3545; /* Vermelho para inativo */
+  background-color: #dc3545; 
   color: white;
 }
 
-/* Responsividade */
 @media (max-width: 768px) {
   .produto-card .row {
     flex-direction: column;
